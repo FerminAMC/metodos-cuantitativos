b55357ad3612dfdcea4a915f7d4a200b266902b6
FerminAMC <a01209665@itesm.mx>
2018-01-14 23:31:48 -0600

    Win the lottery activity done

--- RandomNumbersComerce.py
+++ RandomNumbersComerce.py
@@ -11,18 +11,18 @@ def random_numbers_commerce(n, chance):
             negative_rounding += 1
     return (positive_rounding, negative_rounding)
 # Number of times the program will try the Random Numbers Comerce
-number_of_tries = 50000
+number_of_tries = 5000
 # Chance for positive rounding for the client
 chance = 0.2
 # Extra cents of the object to be purchased
-extra_cents = .47
+extra_cents = .64
 resp = random_numbers_commerce(number_of_tries, chance)
 print "Positive rounding: ", resp[0]
 print "Negative rounding: ", resp[1]
 print "Assuming the product has ", extra_cents, " extra cents"
 if(resp[0] > resp[1]):
-    print "The store has to pay: ", resp[0]*1
-    print "The store saves: ", resp[1]*1
+    print "The store has to pay: ", float(resp[0])*(1-extra_cents)
+    print "The store saves: ", float(resp[1])*(1-extra_cents)
 else:
-    print "The client has to pay: ", resp[1]*1
-    print "The client saves", resp[0]*1
+    print "The client has to pay: ", float(resp[1])*(1-extra_cents)
+    print "The client saves", float(resp[0])*(1-extra_cents)
---
+++ WinTheLottery.py
@@ -0,0 +1,64 @@
+import random
+
+def win_lottery_fixed(placed_bet):
+    right_numbers = [0, 0, 0, 0, 0]
+    rand_list = []
+    while(len(rand_list) < len(placed_bet)):
+        rand_num = random.randint(1, 99)
+        if(rand_num not in rand_list):
+            rand_list.append(rand_num)
+    for i in range(len(placed_bet)):
+        if(placed_bet[i] == rand_list[i]):
+            right_numbers[i] += 1
+        else:
+            break
+    return right_numbers
+
+def win_lottery_rand():
+    right_numbers = [0, 0, 0, 0, 0]
+    rand_list_1 = []
+    rand_list_2 = []
+    # Here I create the first random lottery numbers
+    while(len(rand_list_1) < 5):
+        rand_num = random.randint(1, 99)
+        if(rand_num not in rand_list_1):
+            rand_list_1.append(rand_num)
+    # Here I create the second random lottery numbers
+    while(len(rand_list_2) < 5):
+        rand_num = random.randint(1, 99)
+        if(rand_num not in rand_list_2):
+            rand_list_2.append(rand_num)
+    for i in range(len(right_numbers)):
+        if(rand_list_1[i] == rand_list_2[i]):
+            right_numbers[i] += 1
+        else:
+            break
+    return right_numbers
+
+number_of_tries = 90000
+# Bet placed by the person
+placed_bet = [3, 25, 28, 37, 71]
+# Number of times a person wins the lottery
+winnings_fixed = [0, 0, 0, 0, 0]
+winnings_random = [0, 0, 0, 0, 0]
+
+for i in range(number_of_tries):
+    res_1 = win_lottery_fixed(placed_bet)
+    res_2 = win_lottery_rand()
+    for j in range(5):
+        winnings_fixed[j] += res_1[j]
+        winnings_random[j] += res_2[j]
+
+print "-- FIXED NUMBERS --"
+print "With ", number_of_tries, " tries, the chances of hitting:"
+print "1 number: ", float(winnings_fixed[0])/number_of_tries
+print "2 numbers: ", float(winnings_fixed[1])/number_of_tries
+print "3 numbers: ", float(winnings_fixed[2])/number_of_tries
+print "4 numbers: ", float(winnings_fixed[3])/number_of_tries
+print "5 numbers: ", float(winnings_fixed[4])/number_of_tries
+print "\n-- RANDOM NUMBERS --"
+print "1 number: ", float(winnings_random[0])/number_of_tries
+print "2 numbers: ", float(winnings_random[1])/number_of_tries
+print "3 numbers: ", float(winnings_random[2])/number_of_tries
+print "4 numbers: ", float(winnings_random[3])/number_of_tries
+print "5 numbers: ", float(winnings_random[4])/number_of_tries
